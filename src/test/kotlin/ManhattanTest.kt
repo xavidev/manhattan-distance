da68b9e50ac6fcaab6fcad83caa317deae717894
Xavi Toledano <xavitole@gmail.com>
2022-10-30 01:23:49 +0200

    Here we go again

--- src/test/kotlin/ManhattanTest.kt
+++ src/test/kotlin/ManhattanTest.kt
@@ -1,37 +1,13 @@
 import Point.Point
-import org.junit.jupiter.api.Test
 import kotlin.test.assertTrue
 
 class ManhattanTest {
     private fun assertDistance(p1: Point, p2: Point, expected: Int) {
         assertTrue { manhattanDistance(p1, p2) == expected }
     }
-    @Test
-    fun `distance between same points`() {
-        assertDistance(Point(1, 1), Point(1, 1), 0)
-        assertDistance(Point(3, 3), Point(3, 3), 0)
-        assertDistance(Point(-3, 3), Point(-3, 3), 0)
-    }
-    @Test
-    fun `distance between positive points`() {
-        assertDistance(Point(0, 0), Point(2, 2), 4)
-        assertDistance(Point(2, 2), Point(0, 0), 4)
-    }
 
-    @Test
-    fun `test distance between extreme points`(){
-        assertDistance(Point(-3, 3), Point(3, -3), 12)
-        assertDistance(Point(-2, 1), Point(1, -2), 6)
-        assertDistance(Point(0, 1), Point(1, 0), 2)
-    }
 
     private fun manhattanDistance(p1: Point, p2: Point): Int {
-        if (p1 == p2)
-            return 0
-        if(p1.isInverseOf(p2)){
-            return p1 + p2
-        }
-
-        return 6
+        return 0
     }
 }
--- src/test/kotlin/Point/Point.kt
+++ src/test/kotlin/Point/Point.kt
@@ -1,12 +1,3 @@
 package Point
 
-import java.lang.Math.abs
-
-data class Point(private val x: Int, private val y: Int) {
-    fun isInverseOf(p2: Point): Boolean = swap() == p2
-
-    private fun swap(): Point = copy(x = y, y = x)
-    operator fun plus(p2: Point): Int {
-        return abs(x) * 2 + abs(y) * 2
-    }
-}
+data class Point(private val x: Int, private val y: Int)
