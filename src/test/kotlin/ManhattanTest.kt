437689f0703d59cada0f0728997fbfefc89f7135
Xavi Toledano <xavitole@gmail.com>
2022-10-30 00:28:52 +0200

    Test equal and inverse points

--- src/test/kotlin/ManhattanTest.kt
+++ src/test/kotlin/ManhattanTest.kt
@@ -7,17 +7,31 @@ class ManhattanTest {
         assertTrue { manhattanDistance(p1, p2) == expected }
     }
     @Test
-    fun `test distance between positive points`() {
+    fun `distance between same points`() {
         assertDistance(Point(1, 1), Point(1, 1), 0)
-        assertDistance(Point(2, 2), Point(4, 4), 4)
+        assertDistance(Point(3, 3), Point(3, 3), 0)
+        assertDistance(Point(-3, 3), Point(-3, 3), 0)
     }
     @Test
-    fun `test distance between negative points`() {
-        assertDistance(Point(-3, 3), Point(-3, 3), 0)
-        assertDistance(Point(3, -3), Point(-3, 3), 12)
+    fun `distance between positive points`() {
+        assertDistance(Point(0, 0), Point(2, 2), 4)
+        assertDistance(Point(2, 2), Point(0, 0), 4)
+    }
+
+    @Test
+    fun `test distance between extreme points`(){
+        assertDistance(Point(-3, 3), Point(3, -3), 12)
+        assertDistance(Point(-2, 1), Point(1, -2), 6)
+        assertDistance(Point(0, 1), Point(1, 0), 2)
     }
 
     private fun manhattanDistance(p1: Point, p2: Point): Int {
-        return p1 + p2
+        if (p1 == p2)
+            return 0
+        if(p1.isInverseOf(p2)){
+            return p1 + p2
+        }
+
+        return 6
     }
 }
--- src/test/kotlin/Point/Point.kt
+++ src/test/kotlin/Point/Point.kt
@@ -3,8 +3,10 @@ package Point
 import java.lang.Math.abs
 
 data class Point(private val x: Int, private val y: Int) {
-    fun vector() = abs(x) + abs(y)
-    operator fun plus(p2: Point): Int {
+    fun isInverseOf(p2: Point): Boolean = swap() == p2
 
+    private fun swap(): Point = copy(x = y, y = x)
+    operator fun plus(p2: Point): Int {
+        return abs(x) * 2 + abs(y) * 2
     }
 }
